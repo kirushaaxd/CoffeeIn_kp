d8f5057d889967817a6d59c6d351c53c8fdf179d
Kirill <125600107+kirushaaxd@users.noreply.github.com>
2023-12-09 10:16:01 +0300

    Connect with Firebase

--- app/src/main/java/com/example/coffeein_kp/StaticResources.java
+++ app/src/main/java/com/example/coffeein_kp/StaticResources.java
@@ -1,11 +1,23 @@
 package com.example.coffeein_kp;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.List;
 
 public class StaticResources {
     static List<Object> dishes;
     static List<Object> coffeeHouses;
     static List<Object> orders;
+    static Client currentClient;
 
+    static FirebaseFirestore fBase;
+    static FirebaseAuth mAuth;
+
+    public static void initialize(){
+        fBase = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+    }
 
 }
